202a3be600b983f97af557640cdc65b31d17e4af
bcosters <ctw01914@criticaltechworks.com>
2022-02-18 16:56:22 +0000

    Added some standard tests for the iterators

--- tests/iterator_tests.cpp
+++ tests/iterator_tests.cpp
@@ -6,7 +6,7 @@
 /*   By: bcosters <bcosters@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 12:55:53 by bcosters          #+#    #+#             */
-/*   Updated: 2022/02/18 14:26:14 by bcosters         ###   ########.fr       */
+/*   Updated: 2022/02/18 16:54:18 by bcosters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,41 +27,217 @@ namespace ft = std;
 #include "../include/utility.hpp"
 #endif // _IS_TEST
 #include <iostream>
+#include <numeric>
+#include <sstream>
 #include <stdlib.h>
 #include <vector>
 
-template<long FROM, long TO>
-class Range {
+template <long FROM, long TO> class Range {
 public:
-    // member typedefs provided through inheriting from std::iterator
-    class iterator: public ft::iterator<
-                    ft::input_iterator_tag,   // iterator_category
-                        long,                      // value_type
-                        long,                      // difference_type
-                        const long*,               // pointer
-                        long                       // reference
-                                      >{
-        long	num;
-    public:
+  // member typedefs provided through inheriting from std::iterator
+  class iterator
+      : public ft::iterator<ft::input_iterator_tag, // iterator_category
+                            long,                   // value_type
+                            long,                   // difference_type
+                            const long *,           // pointer
+                            long                    // reference
+                            > {
+    long num;
 
-        explicit iterator(long _num = 0) : num(_num) {}
-        iterator& operator++() {num = TO >= FROM ? num + 1: num - 1; return *this;}
-        iterator operator++(int) {iterator retval = *this; ++(*this); return retval;}
-        bool operator==(iterator other) const {return num == other.num;}
-        bool operator!=(iterator other) const {return !(*this == other);}
-        reference operator*() const {return num;}
-    };
-    iterator begin() {return iterator(FROM);}
-    iterator end() {return iterator(TO >= FROM? TO+1 : TO-1);}
+  public:
+    explicit iterator(long _num = 0) : num(_num) {}
+    iterator &operator++() {
+      num = TO >= FROM ? num + 1 : num - 1;
+      return *this;
+    }
+    iterator operator++(int) {
+      iterator retval = *this;
+      ++(*this);
+      return retval;
+    }
+    bool operator==(iterator other) const { return num == other.num; }
+    bool operator!=(iterator other) const { return !(*this == other); }
+    reference operator*() const { return num; }
+  };
+  iterator begin() { return iterator(FROM); }
+  iterator end() { return iterator(TO >= FROM ? TO + 1 : TO - 1); }
 };
 
-void	tests() {
-    Range<15, 25> range;
- 
-    for(Range<15, 25>::iterator it = range.begin(); it != range.end(); it++) {
-        std::cout << *it << ' '; // print entire range
+class MyIterator : public ft::iterator<ft::input_iterator_tag, int> {
+  int *p;
+
+public:
+  MyIterator(int *x) : p(x) {}
+  MyIterator(const MyIterator &mit) : p(mit.p) {}
+  MyIterator &operator++() {
+    ++p;
+    return *this;
+  }
+  MyIterator operator++(int) {
+    MyIterator tmp(*this);
+    operator++();
+    return tmp;
+  }
+  bool operator==(const MyIterator &rhs) const { return p == rhs.p; }
+  bool operator!=(const MyIterator &rhs) const { return p != rhs.p; }
+  int &operator*() { return *p; }
+};
+
+/**
+ *  @brief  Provides output iterator semantics for streams.
+ *
+ *  This class provides an iterator to write to an ostream.  The type Tp is
+ *  the only type written by this iterator and there must be an
+ *  operator<<(Tp) defined.
+ *
+ *  @tparam  _Tp  The type to write to the ostream.
+ *  @tparam  _CharT  The ostream char_type.
+ *  @tparam  _Traits  The ostream char_traits.
+ */
+template <typename _Tp, typename _CharT = char,
+          typename _Traits = std::char_traits<_CharT> >
+class ostream_iterator
+    : public ft::iterator<ft::output_iterator_tag, void, void, void, void> {
+public:
+  //@{
+  /// Public typedef
+  typedef _CharT char_type;
+  typedef _Traits traits_type;
+  typedef std::basic_ostream<_CharT, _Traits> ostream_type;
+  //@}
+private:
+  ostream_type *_M_stream;
+  const _CharT *_M_string;
+
+public:
+  /// Construct from an ostream.
+  ostream_iterator(ostream_type &__s)
+      : _M_stream(std::__addressof(__s)), _M_string(0) {}
+  /**
+   *  Construct from an ostream.
+   *
+   *  The delimiter string @a c is written to the stream after every Tp
+   *  written to the stream.  The delimiter is not copied, and thus must
+   *  not be destroyed while this iterator is in use.
+   *
+   *  @param  __s  Underlying ostream to write to.
+   *  @param  __c  CharT delimiter string to insert.
+   */
+  ostream_iterator(ostream_type &__s, const _CharT *__c)
+      : _M_stream(&__s), _M_string(__c) {}
+  /// Copy constructor.
+  ostream_iterator(const ostream_iterator &__obj)
+      : _M_stream(__obj._M_stream), _M_string(__obj._M_string) {}
+  /// Writes @a value to underlying ostream using operator<<.  If
+  /// constructed with delimiter string, writes delimiter to ostream.
+  ostream_iterator &operator=(const _Tp &__value) {
+    __glibcxx_requires_cond(
+        _M_stream != 0,
+        _M_message(__gnu_debug::__msg_output_ostream)._M_iterator(*this));
+    *_M_stream << __value;
+    if (_M_string)
+      *_M_stream << _M_string;
+    return *this;
+  }
+  ostream_iterator &operator*() { return *this; }
+  ostream_iterator &operator++() { return *this; }
+  ostream_iterator &operator++(int) { return *this; }
+};
+
+template <typename _Tp, typename _CharT = char,
+          typename _Traits = std::char_traits<_CharT>,
+          typename _Dist = ptrdiff_t>
+class istream_iterator : public ft::iterator<ft::input_iterator_tag, _Tp, _Dist,
+                                             const _Tp *, const _Tp &> {
+public:
+  typedef _CharT char_type;
+  typedef _Traits traits_type;
+  typedef std::basic_istream<_CharT, _Traits> istream_type;
+
+private:
+  istream_type *_M_stream;
+  _Tp _M_value;
+  bool _M_ok;
+
+public:
+  ///  Construct end of input stream iterator.
+  _GLIBCXX_CONSTEXPR istream_iterator()
+      : _M_stream(0), _M_value(), _M_ok(false) {}
+  ///  Construct start of input stream iterator.
+  istream_iterator(istream_type &__s) : _M_stream(std::__addressof(__s)) {
+    _M_read();
+  }
+  istream_iterator(const istream_iterator &__obj)
+      : _M_stream(__obj._M_stream), _M_value(__obj._M_value),
+        _M_ok(__obj._M_ok) {}
+  const _Tp &operator*() const {
+    __glibcxx_requires_cond(
+        _M_ok, _M_message(__gnu_debug::__msg_deref_istream)._M_iterator(*this));
+    return _M_value;
+  }
+  const _Tp *operator->() const { return std::__addressof((operator*())); }
+  istream_iterator &operator++() {
+    __glibcxx_requires_cond(
+        _M_ok, _M_message(__gnu_debug::__msg_inc_istream)._M_iterator(*this));
+    _M_read();
+    return *this;
+  }
+  istream_iterator operator++(int) {
+    __glibcxx_requires_cond(
+        _M_ok, _M_message(__gnu_debug::__msg_inc_istream)._M_iterator(*this));
+    istream_iterator __tmp = *this;
+    _M_read();
+    return __tmp;
+  }
+  bool _M_equal(const istream_iterator &__x) const {
+    return (_M_ok == __x._M_ok) && (!_M_ok || _M_stream == __x._M_stream);
+  }
+
+private:
+  void _M_read() {
+    _M_ok = (_M_stream && *_M_stream) ? true : false;
+    if (_M_ok) {
+      *_M_stream >> _M_value;
+      _M_ok = *_M_stream ? true : false;
     }
-    std::cout << '\n';
+  }
+};
+
+///  Return true if x and y are both end or not end, or x and y are the same.
+template <typename _Tp, typename _CharT, typename _Traits, typename _Dist>
+inline bool
+operator==(const istream_iterator<_Tp, _CharT, _Traits, _Dist> &__x,
+           const istream_iterator<_Tp, _CharT, _Traits, _Dist> &__y) {
+  return __x._M_equal(__y);
+}
+///  Return false if x and y are both end or not end, or x and y are the same.
+template <class _Tp, class _CharT, class _Traits, class _Dist>
+inline bool
+operator!=(const istream_iterator<_Tp, _CharT, _Traits, _Dist> &__x,
+           const istream_iterator<_Tp, _CharT, _Traits, _Dist> &__y) {
+  return !__x._M_equal(__y);
+}
+
+void tests() {
+  std::cout << "\nINPUT_ITERATOR\n";
+  Range<15, 25> range;
+
+  for (Range<15, 25>::iterator it = range.begin(); it != range.end(); it++) {
+    std::cout << *it << ' '; // print entire range
+  }
+  std::cout << '\n';
+
+  int numbers[] = {10, 20, 30, 40, 50};
+  MyIterator from(numbers);
+  MyIterator until(numbers + 5);
+  for (MyIterator it = from; it != until; it++)
+    std::cout << *it << ' ';
+  std::cout << '\n';
+
+  std::cout << "\nOUTPUT_ITERATOR\n";
+  std::istringstream str("0.1 0.2 0.3 0.4");
+  std::partial_sum(istream_iterator<double>(str), istream_iterator<double>(),
+                   ostream_iterator<double>(std::cout, ","));
 }
 
 int main() {
